da67274dab7fab046bf5efda5b9f395f100c8606
Zebediah Figura <z.figura12@gmail.com>
2019-03-03 11:02:28 -0600

    server: Create futex sections for timer objects.

--- dlls/ntdll/fsync.c
+++ dlls/ntdll/fsync.c
@@ -723,6 +723,7 @@ static NTSTATUS __fsync_wait_objects( DWORD count, const HANDLE *handles,
                         break;
                     }
                     case FSYNC_AUTO_EVENT:
+                    case FSYNC_AUTO_SERVER:
                     {
                         struct event *event = obj->shm;
 
@@ -924,6 +925,7 @@ tryagain:
                     break;
                 }
                 case FSYNC_AUTO_EVENT:
+                case FSYNC_AUTO_SERVER:
                 {
                     struct event *event = obj->shm;
                     if (!__sync_val_compare_and_swap( &event->signaled, 1, 0 ))
@@ -970,6 +972,7 @@ tooslow:
                     break;
                 }
                 case FSYNC_AUTO_EVENT:
+                case FSYNC_AUTO_SERVER:
                 {
                     struct event *event = obj->shm;
                     __atomic_store_n( &event->signaled, 1, __ATOMIC_SEQ_CST );
